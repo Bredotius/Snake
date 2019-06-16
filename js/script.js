f0952a42b436d419382b386747a18b3bfe03855f
Bredotius <limurxy@gmail.com>
2019-06-16 18:37:20 +0700

    Имеем полностью функционирующую змейку с системой подсчета очков

--- js/script.js
+++ js/script.js
@@ -59,24 +59,104 @@ function createMouse(){
 
 createMouse();
 
+let direction = 'right';
+let step = false;
+
+let input = document.createElement('input');
+document.body.appendChild(input);
+input.style.cssText = `
+margin: auto;
+margin-top: 40px;
+font-size: 20px;
+display: block;`;
+
+let score = 0;
+input.value = `Ваши очки: ${score}`;
+
 function move(){
   let snakeCoordinates = [snakeBody[0].getAttribute('posX'),snakeBody[0].getAttribute('posY')];
   snakeBody[0].classList.remove('head');
   snakeBody[snakeBody.length-1].classList.remove('snakeBody');
   snakeBody.pop();
 
-  if(snakeCoordinates[0] < 10){
-    snakeBody.unshift(document.querySelector(`[posX = "${+snakeCoordinates[0] + 1}"][posY = "${snakeCoordinates[1]}"]`));
-  }
-  else{
-    snakeBody.unshift(document.querySelector(`[posX = "1"][posY = "${snakeCoordinates[1]}"]`));
+  if(direction == 'right'){
+    if(snakeCoordinates[0] < 10){
+      snakeBody.unshift(document.querySelector(`[posX = "${+snakeCoordinates[0] + 1}"][posY = "${snakeCoordinates[1]}"]`));
+    }
+    else{
+      snakeBody.unshift(document.querySelector(`[posX = "1"][posY = "${snakeCoordinates[1]}"]`));
+    }
+  } 
+  if(direction == 'left'){
+    if(snakeCoordinates[0] > 1){
+      snakeBody.unshift(document.querySelector(`[posX = "${+snakeCoordinates[0] - 1}"][posY = "${snakeCoordinates[1]}"]`));
+    }
+    else{
+      snakeBody.unshift(document.querySelector(`[posX = "10"][posY = "${snakeCoordinates[1]}"]`));
+    }
+  } 
+  if(direction == 'up'){
+    if(snakeCoordinates[1] < 10){
+      snakeBody.unshift(document.querySelector(`[posX = "${snakeCoordinates[0]}"][posY = "${+snakeCoordinates[1] + 1}"]`));
+    }
+    else{
+      snakeBody.unshift(document.querySelector(`[posX = "${snakeCoordinates[0]}"][posY = "1"]`));
+    }
+  } 
+  if(direction == 'down'){
+    if(snakeCoordinates[1] > 1){
+      snakeBody.unshift(document.querySelector(`[posX = "${snakeCoordinates[0]}"][posY = "${+snakeCoordinates[1] - 1}"]`));
+    }
+    else{
+      snakeBody.unshift(document.querySelector(`[posX = "${snakeCoordinates[0]}"][posY = "10"]`));
+    }
+  } 
+
+  if(snakeBody[0].getAttribute('posX') == mouse.getAttribute('posX') &&
+  snakeBody[0].getAttribute('posY') == mouse.getAttribute('posY')){
+    mouse.classList.remove('mouse');
+    let a = snakeBody[snakeBody.length - 1].getAttribute('posX');
+    let b = snakeBody[snakeBody.length - 1].getAttribute('posY');
+    snakeBody.push(document.querySelector(`[posX = "${a}"][posY = "${b}"]`));
+
+    createMouse();
+
+    score++;
+    input.value = `Ваши очки: ${score}`;
   }
 
+  if(snakeBody[0].classList.contains('snakeBody')){
+    alert('GAME OVER');
+    clearInterval(interval);
+  }
 
   snakeBody[0].classList.add('head');
   for(let i = 0; i<snakeBody.length; i++){
     snakeBody[i].classList.add('snakeBody');
   }
+
+  step = true;
 }
 
 let interval = setInterval(move, 300);
+
+window.addEventListener('keydown', function(e){
+  if(step){
+    if(e.keyCode == 37 && direction != 'right'){
+      direction = 'left';
+      step = false;
+    }
+    if(e.keyCode == 38 && direction != 'down'){
+      direction = 'up';
+      step = false;
+    }
+    if(e.keyCode == 39 && direction != 'left'){
+      direction = 'right';
+      step = false;
+    }
+    if(e.keyCode == 40 && direction != 'up'){
+      direction = 'down';
+      step = false;
+    }
+  }
+});
